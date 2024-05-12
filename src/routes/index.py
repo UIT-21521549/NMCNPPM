8ccd399663df0e1c60d93581d9326c7063cf66eb
21522188 <21522188@gm.uit.edu.vn>
2024-05-12 15:15:00 +0700

    add index page

---
+++ src/routes/index.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from flask import request, render_template, make_response, redirect
+from src.database import USER
+
+index_page = Blueprint("index", __name__, url_prefix="/")
+
+
+@index_page.route("/", methods=["GET"])
+def get_index_page():
+    
+    # return login page here
+    return render_template('index.html')
